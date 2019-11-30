f36319a59ecfadb24876efcece99be7ac4455db6
Kamil Celejewski <kamil12283@gmail.com>
2019-11-30 23:00:23 +0100

    [Add] docker-compose, dockerfile and cluster config

---
+++ src/main/java/com/cassandra/cassandraapi/config/ClusterConfig.java
@@ -0,0 +1,79 @@
+package com.cassandra.cassandraapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractClusterConfiguration;
+import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
+
+import java.util.List;
+
+@Configuration
+public class ClusterConfig extends AbstractClusterConfiguration {
+
+    private final String keyspace;
+    private final String hosts;
+    private final Long replicationFactor;
+
+    ClusterConfig(
+            @Value("${spring.data.cassandra.keyspace-name}") String keyspace,
+            @Value("${spring.data.cassandra.contact-points}") String hosts) {
+        this.keyspace = keyspace;
+        this.hosts = hosts;
+        this.replicationFactor = Long.valueOf(System.getenv("REPLICATION_FACTOR"));
+    }
+
+    @Bean
+    @Override
+    public CassandraClusterFactoryBean cluster() {
+
+        RetryingCassandraClusterFactoryBean bean = new RetryingCassandraClusterFactoryBean();
+
+        bean.setAddressTranslator(getAddressTranslator());
+        bean.setAuthProvider(getAuthProvider());
+        bean.setClusterBuilderConfigurer(getClusterBuilderConfigurer());
+        bean.setClusterName(getClusterName());
+        bean.setCompressionType(getCompressionType());
+        bean.setContactPoints(getContactPoints());
+        bean.setLoadBalancingPolicy(getLoadBalancingPolicy());
+        bean.setMaxSchemaAgreementWaitSeconds(getMaxSchemaAgreementWaitSeconds());
+        bean.setMetricsEnabled(getMetricsEnabled());
+        bean.setNettyOptions(getNettyOptions());
+        bean.setPoolingOptions(getPoolingOptions());
+        bean.setPort(getPort());
+        bean.setProtocolVersion(getProtocolVersion());
+        bean.setQueryOptions(getQueryOptions());
+        bean.setReconnectionPolicy(getReconnectionPolicy());
+        bean.setRetryPolicy(getRetryPolicy());
+        bean.setSpeculativeExecutionPolicy(getSpeculativeExecutionPolicy());
+        bean.setSocketOptions(getSocketOptions());
+        bean.setTimestampGenerator(getTimestampGenerator());
+
+        bean.setKeyspaceCreations(getKeyspaceCreations());
+        bean.setKeyspaceDrops(getKeyspaceDrops());
+        bean.setStartupScripts(getStartupScripts());
+        bean.setShutdownScripts(getShutdownScripts());
+        bean.setJmxReportingEnabled(false);
+
+
+        return bean;
+    }
+
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        final CreateKeyspaceSpecification specification =
+                CreateKeyspaceSpecification.createKeyspace(keyspace)
+                        .ifNotExists()
+                        .with(KeyspaceOption.DURABLE_WRITES, true)
+                        .withSimpleReplication(replicationFactor);
+        return List.of(specification);
+    }
+
+    @Override
+    protected String getContactPoints() {
+        return hosts;
+    }
+}
+
